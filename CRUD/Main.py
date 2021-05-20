5372df3a2f4d57c9c1074b12acf5d00747165c8f
0vishal <vishalsalaskar72@gmail.com>
2021-05-20 16:11:23 +0530

    [Vishal] Refactor : Added Docstring and Program head to program

--- CRUD/Main.py
+++ CRUD/Main.py
@@ -1,10 +1,20 @@
-
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-20
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-20
+@Title : Program to perform basic crud operations
+'''
 import CRUD as op
 
 option = int(input("Enter the option : 1: Create 2: Read 3: Update 4: Delete "))
 
 def Main():
-
+    """
+		Description
+ 		Function To insert value in database table stocks data
+	"""
+    try:
         if option==1:
             create = op.CRUD()
             create.create()
@@ -18,6 +28,8 @@ def Main():
             delete = op.CRUD()
             delete.delete()    
         else: 
-            print("Enter correct option")  
+            print("Enter correct option")
+    except ValueError as e:
+        print(e)         
 
 Main()
